77394de0bce35e59108c32fa3aea2a8d589ba74d
whataudoinghere <whataudoinghere@gmail.com>
2018-03-06 17:43:33 +0700

    Revert "Add custom icon pack support"
    This reverts commit a535f291dcb33d20181966eb8b456c29687f112e.

--- src/com/google/android/apps/nexuslauncher/SettingsActivity.java
+++ src/com/google/android/apps/nexuslauncher/SettingsActivity.java
@@ -1,13 +1,9 @@
 package com.google.android.apps.nexuslauncher;
 
-import android.annotation.SuppressLint;
-import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
-import android.app.PendingIntent;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -16,7 +12,6 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.os.Vibrator;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
@@ -26,16 +21,13 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.android.launcher3.BuildConfig;
-import com.android.launcher3.LauncherAppState;
-import com.android.launcher3.LauncherModel;
 import com.android.launcher3.MultiSelectRecyclerViewActivity;
+
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
-import com.android.launcher3.util.LooperExecutor;
 import com.google.android.apps.nexuslauncher.smartspace.SmartspaceController;
 
 public class SettingsActivity extends com.android.launcher3.SettingsActivity implements PreferenceFragment.OnPreferenceStartFragmentCallback {
-    public final static String ICON_PACK_PREF = "pref_icon_pack";
     public final static String SHOW_PREDICTIONS_PREF = "pref_show_predictions";
     public final static String ENABLE_MINUS_ONE_PREF = "pref_enable_minus_one";
     public final static String SMARTSPACE_PREF = "pref_smartspace";
@@ -63,7 +55,6 @@ public class SettingsActivity extends com.android.launcher3.SettingsActivity imp
 
     public static class MySettingsFragment extends com.android.launcher3.SettingsActivity.LauncherSettingsFragment
             implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
-        private CustomIconPreference mIconPackPref;
         private Context mContext;
 
         @Override
@@ -89,8 +80,7 @@ public class SettingsActivity extends com.android.launcher3.SettingsActivity imp
             }
             findPreference(APP_BUILD_DATE_PREF).setSummary(BuildConfig.BUILD_TIME + "\n" + "(" + BuildConfig.BUILD_HOST + "/" + BuildConfig.BUILD_WHOAMI + ")");
 
-            mIconPackPref = (CustomIconPreference) findPreference(ICON_PACK_PREF);
-            mIconPackPref.setOnPreferenceChangeListener(this);
+
 
             Preference vibrate = findPreference(Utilities.VIBRATIONFEEDBACKTEST);
             vibrate.setOnPreferenceClickListener(this);
@@ -138,50 +128,11 @@ public class SettingsActivity extends com.android.launcher3.SettingsActivity imp
         @Override
         public void onResume() {
             super.onResume();
-            mIconPackPref.reloadIconPacks();
         }
 
         @Override
         public boolean onPreferenceChange(Preference preference, final Object newValue) {
             switch (preference.getKey()) {
-                case ICON_PACK_PREF:
-                    ProgressDialog.show(mContext,
-                            null /* title */,
-                            mContext.getString(R.string.state_loading),
-                            true /* indeterminate */,
-                            false /* cancelable */);
-
-                    new LooperExecutor(LauncherModel.getWorkerLooper()).execute(new Runnable() {
-                        @SuppressLint("ApplySharedPref")
-                        @Override
-                        public void run() {
-                            // Clear the icon cache.
-                            LauncherAppState.getInstance(mContext).getIconCache().clear();
-
-                            // Wait for it
-                            try {
-                                Thread.sleep(1000);
-                            } catch (Exception e) {
-                                Log.e("SettingsActivity", "Error waiting", e);
-                            }
-
-                            if (Utilities.ATLEAST_MARSHMALLOW) {
-                                // Schedule an alarm before we kill ourself.
-                                Intent homeIntent = new Intent(Intent.ACTION_MAIN)
-                                        .addCategory(Intent.CATEGORY_HOME)
-                                        .setPackage(mContext.getPackageName())
-                                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                PendingIntent pi = PendingIntent.getActivity(mContext, 0,
-                                        homeIntent, PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_ONE_SHOT);
-                                getContext().getSystemService(AlarmManager.class).setExact(
-                                        AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 50, pi);
-                            }
-
-                            // Kill process
-                            android.os.Process.killProcess(android.os.Process.myPid());
-                        }
-                    });
-                    return true;
                 case SHOW_PREDICTIONS_PREF:
                     if ((boolean) newValue) {
                         return true;
